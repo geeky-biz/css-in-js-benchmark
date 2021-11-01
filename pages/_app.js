7b8fccf8cb48b413260d4f9d9b07e8b395ee49bd
geekybiz <punitsethi@gmail.com>
2021-11-01 11:55:13 +0530

    Handled start render for css modules url via a temporary hack for next ssr issue

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../main.css"
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
--- pages/regularcss.js
+++ pages/regularcss.js
@@ -3,7 +3,7 @@ import styles from "../regularcss.module.css";
 
 
 const App = () => {
-  const [forceRerender, setForceRerender] = useState(0);
+  const [forceRerender, setForceRerender] = useState(1);
   const buttonColors = ['Brown', 'CornflowerBlue', 'DarkGoldenRod', 'DarkOliveGreen', 'DarkSalmon', 'DarkSlateGray', 'IndianRed', 'MidnightBlue', 'Sienna', 'Teal', 'YellowGreen']
   const timer = setTimeout(function(){
     if (forceRerender >= buttonColors.length-1)
@@ -12,18 +12,20 @@ const App = () => {
       setForceRerender(forceRerender + 1)
   }, 1000);
 
-  console.log(forceRerender)
-  console.log('rendering regularcss component')
+  const styleContainer = typeof window === "undefined" ? "container" : styles.container;
+  
   return (
     <div>
       {
         buttonColors.map(function(btnColor,i){
             let btnClasses = [styles.btn, styles.bgTransparent, styles["border" + buttonColors[forceRerender]], styles["color" + buttonColors[forceRerender]]]
             let btnPrimaryClasses = [styles.btn, styles.colorWhite, styles["bg" + buttonColors[forceRerender]], styles["border" + buttonColors[forceRerender]]]
+            const btnClass = typeof window === "undefined" ? "btn bgTransparent borderBrown colorBrown" : btnClasses.join(' ')
+            const btnPrimaryClass = typeof window === "undefined" ? "btn bgBrown colorWhite" : btnPrimaryClasses.join(' ')
           return (
-            <div className={styles.container}>
-            <button className={btnClasses.join(' ')}>Normal Button</button>
-            <button className={btnPrimaryClasses.join(' ')}>Primary Button</button>
+            <div className={`${styleContainer}`}>
+            <button className={`${btnClass}`}>Normal Button</button>
+            <button className={`${btnPrimaryClass}`}>Primary Button</button>
             </div>
           )
         })
